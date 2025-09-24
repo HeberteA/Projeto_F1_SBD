bd69b5f57f6a25bb07a275cec5251cdee25ce916
HeberteA <heberte.2307@gmail.com>
2025-09-24 15:14:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import psycopg2
 from datetime import date
 
 
-st.set_page_config(layout="wide", page_title="F1 Super Analytics Pro", page_icon="f1.png")
+st.set_page_config(layout="wide", page_title="F1 Analytics", page_icon="f1.png")
 F1_PALETTE = ["#ff0800", "#7F7F7F", "#6b0000", "#B1B1B8", "#c52929", "#FFFFFF", "#9c0000"]
 F1_RED = F1_PALETTE[0]
 F1_BLACK = F1_PALETTE[2]
