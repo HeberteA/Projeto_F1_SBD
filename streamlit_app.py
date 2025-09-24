9869ad65a91485ab651b5e51946a6171c07902f9
HeberteA <heberte.2307@gmail.com>
2025-09-24 10:47:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from datetime import date
 
 
 st.set_page_config(layout="wide", page_title="F1 Super Analytics Pro", page_icon="f1.png")
-F1_PALETTE = ["#E10600", "#7F7F7F", "#15151E", "#B1B1B8", "#FFFFFF"]
+F1_PALETTE = ["#E10600", "#7F7F7F", "#410000ff", "#B1B1B8", "#FFFFFF"]
 F1_RED = F1_PALETTE[0]
 F1_BLACK = F1_PALETTE[2]
 F1_GREY = F1_PALETTE[1]
