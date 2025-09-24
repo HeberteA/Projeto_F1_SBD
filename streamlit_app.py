22b80e0586696d3531de04da78fd080f47133b2f
HeberteA <heberte.2307@gmail.com>
2025-09-24 11:23:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def executar_comando_sql(conn, comando, params=None):
 
 @st.cache_data(ttl=60)
 def carregar_todos_os_dados(_conn):
-    st.info("Carregando e preparando dados do banco de dados...")
+
     
     queries = {
         'races': 'select * from races', 
